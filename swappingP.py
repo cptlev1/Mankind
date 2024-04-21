9875c083e35541395d9e6f89261f4aaab17f8a79
cptlev1 <oayush46@gmail.com>
2024-04-21 10:12:34 +0530

    Create swappingP.py
    add

---
+++ swappingP.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+#Swapping
+temp = num1
+num1 = num2
+num2 = temp
+
+#Displaying the swapped integers
+print("After swapping:")
+print("First integer:", num1)
+print("Second integer:", num2)
